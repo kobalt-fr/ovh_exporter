5c2d11b53c2dabc2df928555eab2dea7c10e442c
Laurent Almeras <laurent.almeras@kobalt.fr>
2023-11-07 01:01:24 +0100

    login command for credentials retrieval

---
+++ src/ovh_exporter/auth.py
@@ -0,0 +1,116 @@
+"""OVH authentication utility."""
+import datetime
+import fileinput
+import http.server
+import os.path
+import re
+import subprocess
+import threading
+
+import ovh
+
+from .logger import log
+
+def login(config_dict):
+    """Init an auth process. Consumer key is displayed in console and
+    `env_file` is updated if provided (replace any OVH_CONSUMER_KEY
+    declaration)."""
+    client = ovh.Client(
+        endpoint=config_dict["ovh"]["endpoint"],
+        application_key=config_dict["ovh"]["application_key"],
+        application_secret=config_dict["ovh"]["application_secret"])
+    req = ovh.ConsumerKeyRequest(client=client)
+    req.add_rule("GET", "/me")
+    for service in config_dict["services"]:
+        req.add_rule("GET", f"/cloud/project/{service['id']}")
+        req.add_rule("GET", f"/cloud/project/{service['id']}/quota")
+        req.add_rule("GET", f"/cloud/project/{service['id']}/instance")
+        req.add_rule("GET", f"/cloud/project/{service['id']}/storage")
+        req.add_rule("GET", f"/cloud/project/{service['id']}/usage/current")
+        req.add_rule("GET", f"/cloud/project/{service['id']}/volume")
+    pending_request = req.request("http://localhost:8000/")
+    subprocess.check_call(["xdg-open", pending_request["validationUrl"]])
+    print(f"Perform login in opened browser ({pending_request['validationUrl']})")
+    log.debug("HTTP server starting.")
+    callback_called = threading.Semaphore(1)
+    httpd = http.server.HTTPServer(
+        ('', 8000),
+        CallbackHttpRequestHanderFactory(callback_called))
+    # pylint: disable=consider-using-with
+    callback_called.acquire()
+    def login_callback_server():
+        httpd.serve_forever()
+        log.debug("HTTP server loop ended.")
+    thread = threading.Thread(name="login-callback", target=login_callback_server)
+    thread.daemon = True
+    thread.start()
+    log.debug("HTTP server started.")
+    # wait that callback page is called
+    # pylint: disable=consider-using-with
+    callback_called.acquire()
+    log.debug("HTTP server - callback notification received.")
+    httpd.shutdown()
+    log.debug("HTTP server stopped.")
+    consumer_key = pending_request["consumerKey"]
+    print(f"Login success; consumerKey={consumer_key}")
+    if "env_file" in config_dict and config_dict["env_file"]:
+        update_env_file(config_dict["env_file"], consumer_key)
+
+
+def update_env_file(env_file, consumer_key):
+    """Update environment file with updated OVH_CONSUMER_KEY.
+    
+    A new file is created if it does not exists."""
+    # create an empty file if it does not exists
+    if not os.path.exists(env_file):
+        with open(env_file, mode="tw", encoding="UTF-8") as f:
+            f.write("OVH_CONSUMER_KEY=")
+    # update OVH_CONSUMER_KEY
+    with fileinput.FileInput([env_file], inplace=True) as f:
+        for line in f:
+            # add an 'updated on' comment
+            if f.isfirstline():
+                update_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"# updated on {update_str}")
+            # ignore existing 'updated on' line
+            if re.match(r'# updated on .*', line):
+                continue
+            # replace OVH_CONSUMER_KEY= definition, else keep entry
+            line = re.sub(
+                r'(OVH_CONSUMER_KEY *= *)(.*)',
+                f"OVH_CONSUMER_KEY={consumer_key}", line)
+            print(line, end="")
+    print("auth.env updated.")
+
+
+# pylint: disable=too-few-public-methods
+class CallbackHttpRequestHanderFactory():
+    """Init a CallbackHttpRequestHander with a semaphore."""
+    def __init__(self, semaphore):
+        self._semaphore = semaphore
+
+    def __call__(self, *args, **kwargs):
+        override = dict(kwargs)
+        override["semaphore"] = self._semaphore
+        return CallbackHttpRequestHandler(*args, **override)
+
+
+class CallbackHttpRequestHandler(http.server.BaseHTTPRequestHandler):
+    """Callback for local http server."""
+    def __init__(self, *args, **kwargs):
+        override = dict(kwargs)
+        self._semaphore = override["semaphore"]
+        override.pop("semaphore")
+        super().__init__(*args, **override)
+
+    # pylint: disable=invalid-name
+    def do_GET(self):
+        """Target for redirect url.
+        
+        When response is received, we consider consumerKey is validated."""
+        self.send_response(http.server.HTTPStatus.OK)
+        self.send_header("Content-Type", "text/plain;charset=utf-8")
+        self.end_headers()
+        self.wfile.writelines(["Login success! Go back to your terminal.".encode('utf-8')])
+        self._semaphore.release()
+        log.debug("HTTP server - callback notification sent.")
--- src/ovh_exporter/cli.py
+++ src/ovh_exporter/cli.py
@@ -1,40 +1,77 @@
 """Command line entry-points."""
 import logging
+import os
+import os.path
+import string
 import time
 
 import click
+import dotenv
 from prometheus_client import start_http_server, REGISTRY
 import yaml
 
-from .ovh_client import Configuration, fetch, build_client
-from .logger import init_logging
+from . import auth
 from .collector import OvhCollector
+from .logger import init_logging, log
+from .ovh_client import Configuration, fetch, build_client
 
 
 @click.group("ovh_exporter")
-def main():
-    """Command line entry-point."""
+@click.pass_context
+def main(ctx):
+    """Command line entry-point. Load configuration."""
     init_logging(logging.INFO)
+    with open("config.yaml", encoding="utf-8") as fstream:
+        # Load configuration
+        config_dict = yaml.safe_load(fstream)
+        # Set on context
+        ctx.obj = config_dict
+        # Load environment file if provided
+        if "env_file" in config_dict and \
+                config_dict["env_file"] and \
+                os.path.exists(config_dict["env_file"]):
+            env_file = config_dict["env_file"]
+            log.info("Loading environment file %s", env_file)
+            dotenv.load_dotenv(env_file)
+        # Expand environment variables in config
+        expandvars(config_dict)
 
 
 @main.command("ovh")
-def ovh():
+@click.pass_context
+def ovh(ctx):
     """OVH client test."""
-    with open("config.yaml", encoding="utf-8") as fstream:
-        config_dict = yaml.safe_load(fstream)
+    config_dict = ctx.obj
     service_id = config_dict["services"][0]["id"]
     client = build_client(Configuration.load(config_dict["ovh"]))
     fetch(client, service_id)
 
 
 @main.command("server")
-def server():
+@click.pass_context
+def server(ctx):
     """Exporter startup"""
-    with open("config.yaml", encoding="utf-8") as fstream:
-        config_dict = yaml.safe_load(fstream)
+    config_dict = ctx.obj
     service_id = config_dict["services"][0]["id"]
     client = build_client(Configuration.load(config_dict["ovh"]))
     REGISTRY.register(OvhCollector(client, service_id))
     start_http_server(9100)
     while True:
         time.sleep(5)
+
+
+@main.command("login")
+@click.pass_context
+def login(ctx):
+    """Perform login (retrieve consumerKey). Updated env_file if configured."""
+    config_dict = ctx.obj
+    auth.login(config_dict)
+
+
+def expandvars(dictionary):
+    """Expand environment variable in dictionary."""
+    for k, v in dictionary.items():
+        if isinstance(v, dict):
+            expandvars(v)
+        elif isinstance(v, str):
+            dictionary[k] = string.Template(v).substitute(os.environ)
